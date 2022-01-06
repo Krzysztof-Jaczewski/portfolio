ec2abecf5346525c0a0ae3489cc68ee3b343a989
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2022-01-06 21:09:15 +0100

    added instaled plugins and siteMedadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,19 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    "gatsby-transformer-remark",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: "Web Developer",
+    description: "Web front-end developer",
+    copyright: "this site was created in 2022 ",
+  },
 }
