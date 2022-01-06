1c6e77540704fb5c85986f4956b9d72003faef13
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2022-01-06 21:09:41 +0100

    created 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../componets/Layout"
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <div></div>
+    </Layout>
+  )
+}
+
+export default NotFound
