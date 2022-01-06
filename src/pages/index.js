73abc3cc3e1668649cd30b85a905f7bd4f6fb917
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2022-01-06 21:11:24 +0100

    home page modified

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
+import { Link } from "gatsby"
 import React from "react"
+import Layout from "../componets/Layout"
+import * as styles from "../styles/home.module.css"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = () => {
+  return (
+    <Layout>
+      <section className={styles.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>frond-end designer</h3>
+          <p> lorem ispum </p>
+        </div>
+      </section>
+      <Link to="/projects" className={styles.button}>
+        Check projects
+      </Link>
+    </Layout>
+  )
 }
+
+export default Home
