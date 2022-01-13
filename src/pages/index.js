e452d35d3614529a50cb082e546df24673d8a04e
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2022-01-13 20:37:12 +0100

    Added static image to homesite

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../componets/Layout"
 import * as styles from "../styles/home.module.css"
-
 const Home = () => {
   return (
     <Layout>
@@ -11,13 +10,28 @@ const Home = () => {
           <h2>Design</h2>
           <h3>frond-end designer</h3>
           <p> lorem ispum </p>
+          <Link to="/projects" className={styles.button}>
+            Check projects
+          </Link>
         </div>
+        <img
+          src="/dev.svg"
+          alt="devImage"
+          style={{ maxWidth: "100%", minWidth: "300px" }}
+        />
       </section>
-      <Link to="/projects" className={styles.button}>
-        Check projects
-      </Link>
     </Layout>
   )
 }
 
 export default Home
+
+export const query = graphql`
+  query DevImage {
+    file(relativePath: { eq: "snowman.png" }) {
+      childImageSharp {
+        gatsbyImageData
+      }
+    }
+  }
+`
