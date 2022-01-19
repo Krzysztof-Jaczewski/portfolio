9460a6b5e912801e3c50a61aaf22856f77a3b496
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2022-01-19 22:10:00 +0100

    created tiles in projects

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,24 +1,42 @@
 import { graphql, Link } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import React from "react"
 import Layout from "../../componets/Layout"
-import * as styles from "../../styles/projects.module.css"
+import {
+  portfolio,
+  styledProjects,
+  card,
+  icons,
+  contetnt,
+} from "./projects.module.css"
 
 const Projects = ({ data }) => {
   const projects = data.projects.nodes
   return (
     <Layout>
-      <div className={styles.portfolio}>
+      <div className={portfolio}>
         <h2>Portfolio</h2>
         <h3>Porjects & websites I`ve Created</h3>
-        <div className={styles.projects}>
-          {projects.map(project => (
-            <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
-              <div>
-                <h3>{project.frontmatter.title}</h3>
-                <p>{project.frontmatter.stack}</p>
-              </div>
-            </Link>
-          ))}
+        <div className={styledProjects}>
+          {projects.map(project => {
+            const image = getImage(project.frontmatter.icon.childImageSharp)
+            console.log(image)
+            return (
+              <Link
+                className={card}
+                to={`/projects/${project.frontmatter.slug}`}
+                key={project.id}
+              >
+                <div className={icons}>
+                  <GatsbyImage image={image} alt="picture" />
+                </div>
+                <div className="contetnt">
+                  <h3>{project.frontmatter.title}</h3>
+                  <p>{project.frontmatter.stack}</p>
+                </div>
+              </Link>
+            )
+          })}
         </div>
       </div>
     </Layout>
@@ -38,6 +56,11 @@ export const query = graphql`
           slug
           stack
           title
+          icon {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
         }
       }
     }
