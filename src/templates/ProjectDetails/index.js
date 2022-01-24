005575b2e5ca948de91c185988b5ef50d0c726cc
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2022-01-24 20:43:03 +0100

    Created template page for single poject details

---
+++ src/templates/ProjectDetails/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Layout from "../../componets/Layout"
+import "normalize.css"
+import { details, featured } from "./ProjectDetails.module.css"
+
+const ProjectDetails = () => {
+  return (
+    <Layout>
+      <div className={details}>
+        <h2>title</h2>
+        <h3>stack</h3>
+        <div className={featured}>{/* <img /> */}</div>
+        {/* <div className={html} dangerouslySetInnerHTML={}/> */}
+      </div>
+    </Layout>
+  )
+}
+
+export default ProjectDetails
